77571b699b7aedf05da97a02a676dd7f99d187af
Stephen Corya <stephen@corya.net>
2024-08-16 14:00:12 -0400

    clean up the logs here too (hopefully)

--- initialization.js
+++ initialization.js
@@ -18,13 +18,26 @@ if (process.env.GORDON_ADMIN_TOKEN_FILE) {
     headers.Authorization = `Bearer ${fs.readFileSync (process.env.GORDON_ADMIN_TOKEN_FILE).toString ()}`
 }
 
+// 1. check for all nodes
+// 2. log each new node number i.e. only print 1/7, 2/7, etc.
+// 3. stop checking for new nodes
+// 4. get layout options from docker
+// 5. stage and apply the layout
+
+var foundAllNodes = false;
+var foundNodes = 0;
+
 const garageEvents = new EventEmitter ()
+// called once all nodes have been found
 .once ('allNodes', async (garageNodes) => {
+
+    foundAllNodes = true;
 
     clearInterval (knownNodesWatch);
 
     let modifications = [];
 
+    // read the docker labels for each of the garage nodes
     for (let node of garageNodes) {
         try {
             var swarmNode = await docker.getNode (node.hostname);
@@ -56,6 +69,7 @@ const garageEvents = new EventEmitter ()
         modifications.push (modification);
     }
 
+    // stage the layout modifications
     try {
         var response = await fetch (`http://${process.env.GORDON_ADMIN_ENDPOINT}/v1/layout`, {
             method: "POST",
@@ -72,10 +86,13 @@ const garageEvents = new EventEmitter ()
         process.exit (1);
     }
 })
+// called once all changes have been staged
 .once ('allChanges', async () => {
 
+    // stop watching for changes, they have all been found
     clearInterval (changesWatch);
 
+    // get the layout version
     try {
         var response = await fetch (`http://${process.env.GORDON_ADMIN_ENDPOINT}/v1/layout`, {
             headers: headers
@@ -84,6 +101,8 @@ const garageEvents = new EventEmitter ()
         console.log ('error fetching layout version:', error.message);
         process.exit (1);
     }
+
+    // increment the layout version and apply the changes
     if (response.status == 200) {
         response = await response.json ();
         let version = response.version + 1;
@@ -115,8 +134,10 @@ const garageEvents = new EventEmitter ()
 
 });
 
+// this checks the status of the cluster before any changes have been staged
 let knownNodesWatch = setInterval (async () => {
 
+    // fetch the status (includes number of peer nodes)
     try {
         var response = await fetch (`http://${process.env.GORDON_ADMIN_ENDPOINT}/v1/status`, {
             headers: headers
@@ -125,17 +146,30 @@ let knownNodesWatch = setInterval (async () => {
         console.log ('error fetching status:', error.message);
         return;
     }
+
+    // check that the status was fetched correctly
     if (response.status == 200) {
+        // parse the response as json
         response = await response.json ();
+        // start the nodes count at 0
         let upNodes = 0;
+        // check that nodes are up
         for (let node of response.nodes) {
+            // check that the node is legitimately up
             if (node.isUp && node.hostname) {
+                // increment the number of nodes
                 upNodes++;
             }
         }
-        console.log (`found ${upNodes}/${process.env.GORDON_EXPECTED_NODE_COUNT} garage nodes`);
+        // found nodes is a "global" variable that keeps the logs clean by ignoring a lack of updates
+        // if we have more nodes up than we have acknowledged as found (there will always be at least one up)
+        // the node counts itself as healthy and available, so the number of up nodes will start at one
+        if (upNodes > foundNodes) {
+            console.log (`found ${upNodes}/${process.env.GORDON_EXPECTED_NODE_COUNT} garage nodes`);
+            foundNodes = upNodes;
+        }
+        // all the nodes have been found
         if (upNodes == Number.parseInt (process.env.GORDON_EXPECTED_NODE_COUNT)) {
-            // console.log (response.nodes);
             garageEvents.emit ('allNodes', response.nodes);
         }
     } else {
@@ -143,8 +177,16 @@ let knownNodesWatch = setInterval (async () => {
     }
 }, 2500);
 
+// this watches the cluster for the number of staged changes
 let changesWatch = setInterval (async () => {
 
+    // no need to do anything if all nodes have not been found
+    // there will be no changes staged
+    if (!foundAllNodes) {
+        return;
+    }
+
+    // check the layout for staged changes
     try {
         var response = await fetch (`http://${process.env.GORDON_ADMIN_ENDPOINT}/v1/layout`, {
             headers: headers
@@ -153,9 +195,12 @@ let changesWatch = setInterval (async () => {
         console.log ('error fetching layout changes:', error.message);
         return;
     }
+
     if (response.status == 200) {
+        // parse the response
         response = await response.json ();
 
+        // the number of roles is equal to the number of applied changes
         let numRoles = response.roles.length;
         if (numRoles == Number.parseInt (process.env.GORDON_EXPECTED_NODE_COUNT)) {
             console.log (`found ${numRoles}/${process.env.GORDON_EXPECTED_NODE_COUNT} roles`);
@@ -164,9 +209,13 @@ let changesWatch = setInterval (async () => {
             process.exit (0);
         }
 
+        // che number of changes is equal to the number of staged or proposed layout changes
         let numChanges = response.stagedRoleChanges.length;
+        // this should generally be equal to the number of expected nodes
+        // because all the changes are staged in one fetch
         console.log (`found ${numChanges}/${process.env.GORDON_EXPECTED_NODE_COUNT} staged changes`);
         if (numChanges == Number.parseInt (process.env.GORDON_EXPECTED_NODE_COUNT)) {
+            // trigger the application of changes
             garageEvents.emit ('allChanges');
         }
     } else {
